8a6bc34cab118e2efc9c2e9174a69249e8b80b08
Marius239 <lux.marius@gmail.com>
2016-01-16 13:59:50 +0100

    Update Missing Values
    Marco hat mich darauf hingewiesen, dass der bisherige Code einen Data
    Frame nur mit Faktoren produziert, das ist jetzt geändert.
    
    Im Code ist dnew der Data.Frame mit allen Variablen.
    dnew2 enthält nur Variablen, wo mindestens 50% der Beobachtungen
    vorhanden sind.

--- SWT_Code.R
+++ SWT_Code.R
@@ -10,9 +10,6 @@ setwd("C:/Users/User/Desktop/Master/5th_Semester/Business_Analytics/SWT/")
 d <- read.csv("BADS - SWT - Trainingset - 2015-10-27/BADS - SWT - Trainingset - 2015-10-27.csv", 
               stringsAsFactors = FALSE)
 
-#Convert target variable to factor
-d$churn <- factor(d$churn, labels = c("retain", "churn"))
-
 
 ##################
 # Missing Values #
@@ -20,15 +17,26 @@ d$churn <- factor(d$churn, labels = c("retain", "churn"))
 
 #Convert missings in string variables to NA and then the string to a factor
 na <- function(x){
-  
-  x[x==""]<-NA
-  if(is.character(x)) x <- as.factor(x)
+
+  if(class(x)!="numeric") {
+    x[x==""]<-NA    
+    x <- as.factor(x)
+  }
   
   return(x)
 }
 
-#Apply na to all variables
-dnew <- as.data.frame(apply(d,2,na))  
+dnew <- d
+
+for (i in 1:173){
+  dnew[,i]<-na(dnew[,i])
+  
+}
+
+levels(dnew$churn)<-c("retain","churn")
+
+# Apply na to all variables
+# dnew <- as.data.frame(apply(d,2,na))  
 
 #Compute proportion of NA to check with excel file
 count <- function(x){
@@ -39,5 +47,30 @@ count <- function(x){
 }
 
 missing  <- data.frame(t(apply(dnew,2,count)))
-five     <- subset(missing, select = missing[1,]>0.5)  #Show variables where more than 50% are missing
+
+# Delete all variables with missing >=0.5
+
+five     <- subset(missing, select = missing[1,]>=0.5)  #Show variables where more than 50% are missing
+five
+length(five)
 sort(five)
+
+idx <- missing[1,]>=0.5
+idx <- !idx[1,]
+dnew2 <- dnew[,idx]
+
+#Check: lenght must be 0
+miss_check  <- data.frame(t(apply(dnew2,2,count)))
+five_check <- subset(miss_check, select = miss_check[1,]>=0.5)  #Show variables where more than 50% are missing
+length(five_check)
+hist(as.numeric(miss_check[1,]))
+
+#edit(missing)
+#Outlier Detection and Data Cleaning
+
+#Check Variables with missings \in [10%, 50%)
+ten_check <- subset(miss_check, select = miss_check[1,]>=0.1)  #Show variables where more than 50% are missing
+length(ten_check)
+
+ls(ten_check)
+
