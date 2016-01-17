4c69c7fd09a1972b2dcb2cce6c384b6bf513bcf1
Marius239 <lux.marius@gmail.com>
2016-01-17 18:49:03 +0100

    Sonntag 18:45 - Update Code Missing Values und Datendatei
    Mit diesem Commit laden wir die neuste Version des Codes zu den Missing
    Values hoch.  Der produzierte Data Frame ist auch dabei. Man muss
    'Training_Set.RData' einfach in den Arbeitsspeicher laden und los
    geht's!
    
    Damit ihr den Code nicht durchlaufen lassen müsst, könnt ihr einfach

--- Missing_Values/Missing_Values.R
+++ Missing_Values/Missing_Values.R
@@ -9,40 +9,51 @@ setwd("C:/Users/User/Desktop/Master/5th_Semester/Business_Analytics/SWT/")
 d <- read.csv("BADS - SWT - Trainingset - 2015-10-27/BADS - SWT - Trainingset - 2015-10-27.csv", 
               stringsAsFactors = FALSE)
 
-#Read variable type
+#Read file with variable types
 vars <- read.csv2("C:/Users/User/Desktop/Master/5th_Semester/Business_Analytics/SWT/Variables.csv",
                   stringsAsFactors = FALSE)
 
-#Store data to ensure original set is not altered
+#Store data to ensure original set is not changed
 dnew <- d
 
-#---------------------------------------------------------------------
-#Convert variable type
+
+#-------------------------------------------------------------------------------
+#Convert variable types of data to type in Excel Dexcription and 
+#Merge blanks and NAs of factor variables to level "Missing"
 
 for(i in 1:ncol(dnew)){
   
-  var  <- colnames(dnew)[i]
-  ind  <- which(vars$Variable == colnames(dnew)[i])
-  type <- vars[ind,2]
+  var  <- colnames(dnew)[i]  #Store variable name
   
+  #Find osition of 'var' in 'vars'
+  ind  <- which(vars$Variable == colnames(dnew)[i])  #Position  
+  type <- vars[ind,2]  #Get variable type
+  
+  #Ensure continuous variables are class numeric
   if(type == "Continuous")  dnew[,i] <- as.numeric(dnew[,i])
   
+  #Ensure Categorical variables are class factor
   if(type == "Categorical"){
     
+    #Convert to factor and keep NAs as level
     dnew[,i] <- factor(dnew[,i], exclude = NULL)
+    
+    #Set <NA> level as "Missing" (converts values as well)
     if(is.na(levels(dnew[,i])[length(levels(dnew[,i]))])){
       
       levels(dnew[,i])[length(levels(dnew[,i]))] <- "Missing"
     }
+    
+    #Merge blank level and <NA> level as jointly "Missing"
     if(levels(dnew[,i])[1] == "") levels(dnew[,i])[1] <- "Missing"
     
     
   }
 }
 
-#---------------------------------------------------------------------
-#Count function to count 'Missing' in Factors and NAs in numericals
 
+#-------------------------------------------------------------------------------
+#Count function to count 'Missing' in Factors and NAs in numericals
 
 count<- function(x){
   
@@ -55,50 +66,69 @@ count<- function(x){
     z <- 1-length(na.omit(x))/length(x)
     return(z)
   }
-  
-  #Count rows
-  if(typeof(x) == "character"){
-    num_na      <- sum(is.na(dnew[i,])) 
-    z <- num_na/length(x)
-    return(z)
-  }
-  
+
 }
 
 
 #-------------------------------------------------------------------------------
-#Check for vertical NAs
+#Compute proportion of missing values for all variables
 
 missing_var <- sapply(dnew, count)
 
 
 #-------------------------------------------------------------------------------
-#Check for horizontal NAs
+# Delete all Variables with Missing Values >= 50%
 
-#Create data set count NAs faster but use information to subset 'dnew'
-dnew_hoz <- dnew
+five     <- missing_var[missing_var>=0.5]  # Contains variables where more than 50% are missing
+length(five) #22 Variables with at least 50% NAs
+
+idx <- missing_var < 0.5  #Index for variables that have more than 50% of observations
+dnew2 <- dnew[,idx]  # Dataset without Variables having more thant 50% Missings 
+
+
+#-------------------------------------------------------------------------------
+#Check for horizontal NAs, i.e. NAs per individual (DELETE IN NEXT SECTION)
+
+#Create data set to count NAs faster but use information to subset 'dnew'
+dnew_hoz <- dnew2
 
 #Coerce 'Missing' in factors to NA
-factors <- which(sapply(dnew_hoz, is.factor))
-
-#sapply(dnew_hoz[,factors], levels)
+factors <- which(sapply(dnew_hoz, is.factor))  #Find position of factors
 
 for(i in factors){
   
-  miss_idx <- levels(dnew_hoz[,i]) == "Missing"
+  miss_idx <- levels(dnew_hoz[,i]) == "Missing"  #Find where 'Missing' is
   
+  #If 'Missing' is contained, coerce to NA
   if(sum(miss_idx) == 1){
-    id <- which(miss_idx)
-    levels(dnew_hoz[,i])[id] <- NA
+    id <- which(miss_idx)  #Position of 'Missing'
+    levels(dnew_hoz[,i])[id] <- NA  #Coerce to NA
   }
   
 }
 
-#Count
-n <- nrow(dnew_hoz)
-missing_ind <- rep(NA, n)
+#Count horizontal missings
+n     <- nrow(dnew_hoz)  #Number of individuals
+n_var <- ncol(dnew_hoz)  #Number of variables
+missing_ind <- rep(NA, n)  #Vector to safe result
 for(i in 1:n){
   
-  num_na      <- sum(is.na(dnew_hoz[i,]))
-  missing_ind[i] <-num_na/n  
+  num_na         <- sum(is.na(dnew_hoz[i,]))  #Sum of NAs
+  missing_ind[i] <- num_na/n_var  #Share of NAs
 }
+
+
+#------------------------------------------------------------------------------------
+# Delete Observations with too many Missing Values
+
+ten <- missing_ind[missing_ind>=0.1]
+length(ten)  #1080 individuals with at least 10% NAs
+
+idx <- missing_ind < 0.1
+dnew3 <- dnew2[idx,]  # Dataset without Observations with at least 10% Missings 
+
+
+#-------------------------------------------------------------------------------------
+# Dataset without Missing Values in Continuous Variables
+
+data <- na.omit(dnew3)
